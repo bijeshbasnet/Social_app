195d7b9aba6228e0231911669003cd43784fcbb6
bijeshbasnet <bijeshjungbasnet@gmail.com>
2020-09-24 23:15:39 +0545

    Build the image model

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -12,11 +12,14 @@ SECRET_KEY = 'ru-3skdj5#o=27&xt-_%nyr*8rzv4u9d@nvv7m8gr-ounk6+=p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
 INSTALLED_APPS = [
+    'django_extensions',
+    'social_django',
+    'images.apps.ImagesConfig',
     'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -48,7 +51,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+                'django.contrib.messages.context_processors.messages'
             ],
         },
     },
@@ -85,7 +88,19 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+    'social_core.backends.facebook.FacebookOAuth2',
+    'social_core.backends.google.GoogleOAuth2',
+]
 
 
 # Internationalization
@@ -105,6 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SOCIAL_AUTH_FACEBOOK_KEY = ' 362135011501989'  # Facebook App ID
+SOCIAL_AUTH_FACEBOOK_SECRET = '94ae352568a31e8588155537a0a99f09'  # Facebook App Secret
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '505214541336-dj0b24abs68p7a3o6coq3fcpf0b6hnk9.apps.googleusercontent.com'  # Google Consumer Key
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'NM9LL-KI0oqMzTEz9MDsMBJP'  # Google Consumer Secret
+
 
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
